ed58499df3747a88125d9b4722aacbef54d822e6
NICOLAS SOARES DE MATTOS <53660486809@info.local>
2026-03-06 20:20:21 -0300

    calcular media

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
 def limpar_dados () :
 # Retorne uma lista apenas com int ou float
     pass
-def calcular_media () :
-    pass
+def calcular_media (dados) :
+    return sum(dados) / len(dados)
 def calcular_mediana () :
     pass
 def calcular_variancia () :
@@ -13,3 +13,5 @@ def obter_extremos () :
     pass
 dados = limpar_dados ( dados_sujos )
 print ( f" Dados processados : { dados }")
+
+print(f"verificado por nicolas")
